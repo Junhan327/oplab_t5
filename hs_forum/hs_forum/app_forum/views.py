e3906b382b1d87b01a6459dc357d0d39d754dcc0
happysheep <166403966+Junhan327@users.noreply.github.com>
2024-10-19 23:06:57 +0800

    Delete hs_forum directory

--- hs_forum/hs_forum/app_forum/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,HttpResponse
-from app_forum import models
-# Create your views here.
-def login(request):
-    return render(request,"login.html")
-
-def test(request):
-    models.user_info.objects.create(name='test',password='123')
-    return HttpResponse("连接成功")
